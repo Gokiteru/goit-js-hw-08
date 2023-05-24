c3c741493a5df6813c57623e91c600a60757df98
Gokiteru <vakin.vlad@gmail.com>
2023-05-24 20:31:59 +0300

    done 2&3 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const currentTime = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+setCurrentTime()
+
+function onPlay ({ seconds }) {
+    localStorage.setItem(currentTime, seconds)
+}
+
+function setCurrentTime(){
+    if(!localStorage.getItem(currentTime)){
+        return
+    }
+    player.setCurrentTime(localStorage.getItem(currentTime))
+}
